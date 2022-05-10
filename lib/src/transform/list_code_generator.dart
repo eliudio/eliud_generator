aa8cbd3bce13de766db2d1f5cbe507dd400fcd59
Eliud <58745656+eliudio@users.noreply.github.com>
2022-05-10 12:18:21 +0100

    BackgroundModel formatting perfected

--- lib/src/transform/list_code_generator.dart
+++ lib/src/transform/list_code_generator.dart
@@ -248,7 +248,7 @@ class \${id}ListWidgetState extends State<\${id}ListWidget> {
   
   Widget theList(BuildContext context, values, AccessState accessState) {
     return Container(
-      decoration: widget.listBackground == null ? StyleRegistry.registry().styleWithApp(widget.app).adminListStyle().boxDecorator(widget.app, context, accessState.getMember()) : BoxDecorationHelper.boxDecoration(accessState.getMember(), widget.listBackground),
+      decoration: widget.listBackground == null ? StyleRegistry.registry().styleWithApp(widget.app).adminListStyle().boxDecorator(widget.app, context, accessState.getMember()) : BoxDecorationHelper.boxDecoration(widget.app, accessState.getMember(), widget.listBackground),
       child: ListView.separated(
         separatorBuilder: (context, index) => StyleRegistry.registry().styleWithApp(widget.app).adminListStyle().divider(widget.app, context),
         shrinkWrap: true,
